d7a078fea51f8e53eabfd88233eb497073bbcf2d
Aracely Alba <aracelyalba8@gmail.com>
2023-04-10 22:38:10 -0400

    Added fetching API

--- prueba-01/src/App.jsx
+++ prueba-01/src/App.jsx
@@ -2,24 +2,46 @@ import React, { useState, useEffect } from 'react';
 
 const CAT_ENDPOINT_RAMDOM_FACT = `https://catfact.ninja/fact`;
 
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+
 
 function App() {
 
 	const [fact, setFact] = useState('');
+	const [imageUrl, setImageUrl] = useState('');
 
 	useEffect(() => {
 
 		fetch(CAT_ENDPOINT_RAMDOM_FACT)
-			.then(res => res.json())
-			.then(data => setFact(data.fact))
+			.then((res) => res.json())
+			.then((data) => {
+
+				const { fact } = data;
+				setFact(fact);
+			});
 
 	}, []);
+
+	useEffect(() => {
+
+		if(!fact) return;
+		const threeFirstWords = fact.split(' ', 3).join(' ');
+
+		fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+			.then((res) => res.json())
+			.then((rs) => {
+
+				const { url } = rs;
+				setImageUrl(url);
+			});
+
+	}, [fact]);
 
   return (
   	
   	<main>
   		{ fact && <p>{fact}</p> }
+  		{ imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} width="350px"/> }
   	</main>
 
   );
